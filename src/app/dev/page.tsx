d47f38f97f341b863edde3e640885d6be220c421
Adebayo Temitope <tsadebayo@myseneca.ca>
2024-04-01 00:55:31 -0400

    fixes

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -2,17 +2,17 @@ const Dev = () => {
   return (
     <div className="flex flex-col mx-auto gap-2 max-w-3xl text-lg my-16 p-4">
       <p>Hi,</p>
-      <p>I'm <b className="text-xl">Temitope Adebayo</b>, a Software Developer living in Toronto, Ontario</p>
+      <p>I&apos;m <b className="text-xl">Temitope Adebayo</b>, a Software Developer living in Toronto, Ontario</p>
       <p>
-        I have the professional experience of being a Frontend and Fullstack Engineer in multiple industries. I'm currently interested in core engineering (integrating software with hardware)
+        I have the professional experience of being a Frontend and Fullstack Engineer in multiple industries. I&apos;m currently interested in core engineering (integrating software with hardware)
       </p>
       <p>
-        My personal projects can be found on my <i className="text-xl"><b><a className="link" href="">GitHub</a></b></i> and bigger personal projects through <i className="text-xl"><b><a href="" className="link">Nrma</a></b></i>
+        My personal projects can be found on my <i className="text-xl"><b><a className="link" href="https://github.com/tasvln">GitHub</a></b></i> and bigger personal projects through <i className="text-xl"><b><a href="https://github.com/nrmaco" className="link">Nrma</a></b></i>
       </p>
       <p className="uppercase mt-6">my top 5 projects</p>
       <ul className="capitalize list-disc">
         <li><a className="link" href="https://www.projectstadium.com">project stadium (esport tournament platform)</a></li>
-        <li><a className="link" href="">assembly station (C++)</a></li>
+        <li><a className="link" href="https://github.com/tasvln/assembly-station-app">assembly station (C++)</a></li>
         <li><a className="link" href="https://www.madeavln.com">madeavln (shopify)</a></li>
         <li><a className="link" href="https://www.culturemedia.com">culturemedia</a></li>
         <li><a className="link" href="https://www.ade.supply">ade.supply (portfolio)</a></li>
@@ -33,7 +33,7 @@ const Dev = () => {
         <li>Python, C++</li>
         <li>Webassembly</li>
       </ul>
-      <p className="capitalize mt-6">i'm open to learning new technologies//stacks :)</p>
+      <p className="capitalize mt-6">i&apos;m open to learning new technologies//stacks :)</p>
       <p className="uppercase mt-6">can be reached at <b><a className="link" href="mailto:tasvln@outlook.com">tasvln@outlook.com</a></b></p>
     </div>
   );
