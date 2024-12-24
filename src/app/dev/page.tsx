d0d1a67ee4b0d379d3821f551ff522bb6f5e921a
Adebayo Temitope <tsadebayo@myseneca.ca>
2024-12-24 11:58:53 -0500

    update portfolio

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -7,24 +7,24 @@ const Dev = () => {
         I have the professional experience of being a Frontend and Fullstack Engineer. I&apos;m currently interested in core engineering (integrating software with hardware) but open to opportunities/taking a challenge.
       </p>
       <p>
-        My personal projects can be found on my <i className="text-xl"><b><a className="link" href="https://github.com/tasvln">GitHub</a></b></i> and other projects through <i className="text-xl"><b><a href="https://github.com/nrmaco" className="link">Nrma</a></b></i>
+        My personal projects can be found on my <i className="text-xl"><b><a className="link" href="https://github.com/tasvln">GitHub</a></b></i>
       </p>
       <p className="uppercase mt-6">my top projects</p>
       <ul className="capitalize list-disc">
+        <li><a className="link" href="https://www.nrma.company">the nrma company</a></li>
         <li><a className="link" href="https://www.projectstadium.com">project stadium (esport tournament platform)</a></li>
         <li><a className="link" href="https://www.disyl.ade.supply">disyl (vinyl editor)</a></li>
         {/* <li><a className="link" href="https://www.grit.ade.supply">grit ()</a></li> */}
         <li><a className="link" href="https://www.placeonatee.ade.supply">place-on-a-tee (tshirt editor)</a></li>
         <li><a className="link" href="https://github.com/tasvln/assembly-station-app">assembly station (C++)</a></li>
-        <li><a className="link" href="https://www.madeavln.com">madeavln (shopify)</a></li>
-        <li><a className="link" href="https://www.culturemedia.com">culturemedia (nft marketplace)</a></li>
+        <li><a className="link" href="https://www.madeavln.com">madeavln (Headless shopify)</a></li>
         <li><a className="link" href="https://www.ade.supply">ade.supply (portfolio)</a></li>
       </ul>
       <p className="uppercase mt-6">oop (Object-Oriented Programmingg)</p>
       <ul className="capitalize list-disc">
-        <li>C/C++, Python, Go</li>
+        <li>C++, Python, Go</li>
       </ul>
-      <p className="uppercase mt-6">my front//back-end stack</p>
+      <p className="uppercase mt-6">my front/back-end stack</p>
       <ul className="capitalize list-disc">
         <li>HTML, CSS</li>
         <li>SCSS, Tailwind</li>
@@ -34,6 +34,7 @@ const Dev = () => {
         <li>SQL, NoSQL, MongoDB, Postgresql</li>
         <li>azure, AWS</li>
         <li>Framer Motion, WebGL, ThreeJS</li>
+        <li>Shopify, LiquidJS, </li>
       </ul>
       <p className="uppercase mt-6">tools</p>
       <ul className="capitalize list-disc">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <main className="w-full">
       <div className="flex flex-col gap-2 justify-center items-center h-dvh text-lg">
-        <Link href="">shop [pending]</Link>
+        {/* <Link href="">shop [in development]</Link> */}
         <Link href="/dev">tech portfolio</Link>
         {/* <Link href="">designs [archive]</Link> */}
         {/* <Link href="">beats [::link changes quaterly::]</Link> */}
-        <Link href="">videography [pending]</Link>
+        {/* <Link href="">videography [pending]</Link> */}
         <Link href="mailto:tasvln@outlook.com">contact</Link>
       </div>
     </main>
