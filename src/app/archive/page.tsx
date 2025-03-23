25a5a94715d68cea123ee108a41b2dff3e65b1e2
Adebayo Temitope <tsadebayo@myseneca.ca>
2025-03-23 11:25:24 -0400

    added design section

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
 import archive from "@/data/archive.json"
+import Image from "next/image";
 
 const Archive = () => {
   return (
-    <div className="mx-auto max-w-[1440px] p-8 border-2 border-red-500 relative">
-      <div className="fixed top-0 right-0 p-4 flex flex-col text-right">
+    <div className="mx-auto max-w-[1440px] p-24 m-4 relative">
+      <div className="fixed top-0 right-0 p-4 flex flex-col text-right z-50">
         <Link href="https://instagram.com/byoladimeji" className="uppercase font-bold">Instagram</Link>
         <Link href="https://instagram.com/byoladimeji" className="uppercase font-bold"><sup>(tap in)</sup> commisions</Link>
       </div>
       <div className="grid grid-cols-2 gap-4">
         {archive.map((arc, index) => (
           <div key={index}>
-            <div className="bg-indigo-800 w-full h-[775px]" />
+            <div className="bg-indigo-800 w-full h-[700px] relative">
+              <Image 
+                src={arc.prev}
+                alt={arc.name}
+                fill
+              />
+            </div>
           </div>
         ))}
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex flex-col gap-2 justify-center items-center h-dvh text-lg">
         {/* <Link href="">shop [in development]</Link> */}
         <Link href="/dev">tech portfolio</Link>
-        {/* <Link href="/archive">design [archive]</Link> */}
+        <Link href="/archive">design [archive]</Link>
         {/* <Link href="">beats [::link changes quaterly::]</Link> */}
         {/* <Link href="">videography [pending]</Link> */}
         <Link href="mailto:tsad3bayo@outlook.com">contact</Link>
