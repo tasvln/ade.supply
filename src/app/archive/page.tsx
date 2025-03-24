29c96f232f4a5b908f4d2f963b068c6a05103456
Adebayo Temitope <tsadebayo@myseneca.ca>
2025-03-24 13:58:53 -0400

    fixed responsiveness

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -4,23 +4,21 @@ import Image from "next/image";
 
 const Archive = () => {
   return (
-    <div className="mx-auto max-w-[1440px] p-24 m-4 relative">
+    <div className="mx-auto max-w-[1440px] py-24 px-4 md:p-24 m-4 relative">
       <div className="fixed top-0 right-0 p-4 flex flex-col text-right z-50">
         <Link href="https://instagram.com/byoladimeji" className="uppercase font-bold">Instagram</Link>
         <Link href="https://instagram.com/byoladimeji" className="uppercase font-bold"><sup>(tap in)</sup> commisions</Link>
       </div>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {archive.map((arc, index) => (
           <div key={index}>
-            <div className="bg-gray-50 w-full h-[700px] relative">
-              <Image 
-                src={arc.prev}
-                alt={arc.name}
-                className="object-contain"
-                priority
-                fill
-              />
-            </div>
+            <Image
+              src={arc.prev}
+              alt={arc.name}
+              priority
+              width={1080}
+              height={1350}
+            />
           </div>
         ))}
       </div>
